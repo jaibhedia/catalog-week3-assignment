e69ab018718cb4697dd6083d0f3499c93608cd81
jaibhedia <jaibhedia@gmail.com>
2025-03-06 14:05:14 +0530

    fixed bugs involving unused variables

--- api/src/jobs/mod.rs
+++ api/src/jobs/mod.rs
@@ -1,20 +1,17 @@
-use crate::fetcher::{fetch_depth_data, fetch_swaps_data}; // Keep if used elsewhere
-use crate::services::DepthService;
+use crate::fetcher::{fetch_depth_data, fetch_swaps_data};
 use deadpool_postgres::Pool;
 use tokio_cron_scheduler::{JobScheduler, Job};
 
 pub async fn setup_jobs(pool: Pool) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let sched = JobScheduler::new().await?;
     let client = reqwest::Client::new();
-    let service = DepthService::new(pool.clone());
 
-    let job = Job::new_async("0 0 * * * *", move |_, _| { // Hourly schedule
+    let job = Job::new_async("0 0 * * * *", move |_, _| {
         let client = client.clone();
-        let service = service.clone();
+        let pool = pool.clone();
         Box::pin(async move {
-            service.fetch_and_store_depths(&client).await.unwrap_or_else(|e| eprintln!("Depth error: {}", e));
-            service.fetch_and_store_swaps(&client).await.unwrap_or_else(|e| eprintln!("Swaps error: {}", e));
-            // Add earnings and runepool when implemented
+            fetch_depth_data(&pool, &client).await.unwrap_or_else(|e| eprintln!("Depth error: {}", e));
+            fetch_swaps_data(&pool, &client).await.unwrap_or_else(|e| eprintln!("Swaps error: {}", e));
         })
     })?;
 
